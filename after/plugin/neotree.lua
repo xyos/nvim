12a4ab7572d6b1b6c3cda36ffc6eebdc2978dd5d
Jairo Suarez <jairos@blanclabs.com>
2023-01-22 16:09:00 -0500

    feat: adding neotree keybindings

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -1,2 +1,4 @@
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
-vim.keymap.set("n", "<Leader>n", ":NeoTreeFloatToggle<cr>")
+vim.keymap.set("n", "<Leader>tt", ":Neotree float filesystem reveal_force_cwd<cr>", {noremap = true, silent = true})
+vim.keymap.set("n", "<Leader>tg", ":Neotree float git_status<cr>", {noremap = true, silent = true})
+vim.keymap.set("n", "<Leader>tb", ":Neotree float buffers<cr>", {noremap = true, silent = true})
--- lua/xyos/remap.lua
+++ lua/xyos/remap.lua
@@ -10,10 +10,10 @@ vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 -- Resize with arrows
-vim.keymap.set("n", "<A-Up>", ":resize -2<CR>", silent)
-vim.keymap.set("n", "<A-Down>", ":resize +2<CR>", silent)
-vim.keymap.set("n", "<A-Left>", ":vertical resize -2<CR>", silent)
-vim.keymap.set("n", "<A-Right>", ":vertical resize +2<CR>", silent)
+vim.keymap.set("n", "<A-Up>", ":resize -2<CR>", {silent = true})
+vim.keymap.set("n", "<A-Down>", ":resize +2<CR>", {silent = true})
+vim.keymap.set("n", "<A-Left>", ":vertical resize -2<CR>", {silent = true})
+vim.keymap.set("n", "<A-Right>", ":vertical resize +2<CR>", {silent = true})
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -50,7 +50,6 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/code/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 -- Move text up and down
 vim.keymap.set("n", "<C-k>", "<Esc>:m .-2<CR>==")
