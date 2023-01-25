bf1b3a3f11ca73de411b6a280a2006b6da7360a3
Jairo Suarez <jairos@blanclabs.com>
2023-01-25 05:42:50 -0500

    fix: fix toggleterm mappings

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,8 +1,17 @@
-require('toggleterm').setup()
+require('toggleterm').setup({ cmd = 'fish' })
 
 local Terminal  = require('toggleterm.terminal').Terminal
 
-local fish = Terminal:new({
+local bottom = Terminal:new({ direction = 'horizontal', size = 15, cmd = 'fish' })
+
+function _G.__bterm_toggle()
+  bottom:toggle()
+end
+
+vim.api.nvim_set_keymap('n', '<leader>1', '<cmd>lua __bterm_toggle()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<leader>1', '<C-\\><C-n><cmd>lua __bterm_toggle()<CR>', { noremap = true, silent = true })
+
+local float = Terminal:new({
   cmd = 'fish',
   hidden = true,
   direction = 'float',
@@ -19,7 +28,7 @@ local fish = Terminal:new({
 })
 
 function _G.__fterm_toggle()
-  fish:toggle()
+  float:toggle()
 end
 
 vim.api.nvim_set_keymap('n', '<leader>`', '<cmd> lua __fterm_toggle()<CR>', { noremap = true, silent = true })
@@ -27,8 +36,7 @@ vim.api.nvim_set_keymap('t', '<leader>`', '<C-\\><C-n><cmd> lua __fterm_toggle()
 
 function _G.set_terminal_keymaps()
   local opts = {buffer = 0}
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', 'jj', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-c>', [[<C-\><C-n>]], opts)
   vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
   vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
   vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
